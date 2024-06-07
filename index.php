e76d991f3215704c78e1a538b820458e42afea15
a7comma <iba7comma@gmail.com>
2024-06-07 18:16:53 +0500

    выполнение практики 19

--- index.php
+++ index.php
@@ -1,35 +1,52 @@
 <?php
 
-$title = "title";
-$h1 = "h1";
-$year = date("Y");
+gallery_log();
 
-function getTime(){
-    $hours = (int)date("H");
-    $minutes = (int)date("i");
-
-    $resultTime = $hours;
-
-    if ($hours == 1 || $hours == 21) 
-        $resultTime .= " час ";
-    else if (($hours >=5 && $hours <=20) || ($hours == 0))
-        $resultTime .= " часов ";
-    else $resultTime .= " часа ";
-
-    $resultTime .= $minutes;
-    
-    $divMinutes = $minutes % 10;
-    if ($divMinutes == 0)  
-        $divMinutes = 10; 
-
-    if (($minutes >=11 && $minutes <= 14) || ($divMinutes >= 5 && $divMinutes <= 10))
-        $resultTime .= " минут";
-    else if ($divMinutes == 1) 
-        $resultTime .= " минута";
-    else $resultTime .= " минуты";
-
-    return $resultTime;
+function gallery_log() {
+    $log = date("Y-m-d H:i:s") . PHP_EOL;
+    $path = "logs/log.txt";
+    if (count(file($path)) > 9) {
+        $logsNumber = count(array_slice(scandir('logs'), 2)) - 1;
+        rename($path, "logs/log" . $logsNumber . ".txt");
+    }
+    file_put_contents($path, $log, FILE_APPEND);
 }
+
+function createGallery()
+{
+    foreach (array_slice(scandir("img/big"), 2) as $image) {
+        echo '<a target="_blank" href="img/big/' . $image . '">
+        <img src="img/small/' . $image . '" alt="image"></a>';
+    }
+}
+
+function checkFile() {
+    $imgName = "img/big/" . basename($_FILES["img_file"]["name"]);
+    if (file_exists($imgName)) {
+        echo "Файл с таким названием уже существует!";
+        return false;
+    }
+    $imgSize = getimagesize($_FILES["img_file"]["tmp_name"]);
+    if ($imgSize['mime'] != 'image/jpeg') {
+        echo "Файл должен быть формата jpg!";
+        return false;
+    }
+    if ($_FILES["img_file"]["size"] > 1024 * 1024 * 5) {
+        echo "Размер файла не должен превышать 5 МБ!";
+        return false;
+    }
+    return true;
+}
+
+if (!empty($_FILES)) {
+    $path = "img/big/" . $_FILES["img_file"]["name"];
+    if (checkFile() && move_uploaded_file($_FILES["img_file"]["tmp_name"], $path)){
+        $img = imagecreatefromjpeg($path);
+        $imgScale = imagescale($img , 200);
+        imagejpeg($imgScale, "img/small/" . $_FILES["img_file"]["name"]);
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -37,11 +54,13 @@ function getTime(){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title ?></title>
+    <title>lesson19</title>
 </head>
 <body>
-    <h1><?php echo $h1 ?></h1>
-    <p><?php echo $year ?></p>
-    <p><?php echo getTime() ?></p>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="img_file">
+        <input type="submit" value="Загрузить">
+    </form>
+    <?php createGallery() ?>
 </body>
 </html>
