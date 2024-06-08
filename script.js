86c19cb5f9ad3d61931e50291ace380523f7161c
a7comma <iba7comma@gmail.com>
2024-06-08 15:18:32 +0500

    выполнение практики 20

--- script.js
+++ script.js
@@ -5,90 +5,11 @@ if (document.readyState === 'loading') {
 }
 
 function init() {
-    const data = {
-        name: 'Каталог товаров',
-        hasChildren: true,
-        items: [
-            {
-                name: 'Мойки',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    },
-                    {
-                        name: ' Vigro Mramor',
-                        hasChildren: false,
-                        items: []
-                    },
-                    {
-                        name: 'Handmade',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    },
-                    {
-                        name: ' Vigro Glass',
-                        hasChildren: false,
-                        items: []
-                    }
-                ]
-            },{
-                name: 'Фильтры',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    },
-                    {
-                        name: ' Vigro Mramor',
-                        hasChildren: false,
-                        items: []
-                    }
-                ]
-            }
-        ]
-    }
-
+    const data = null
 
     const items = new ListItems(document.getElementById('list-items'), data)
 
+    items.render()
     items.init()
 
     function ListItems(el, data) {
@@ -96,7 +17,6 @@ function init() {
         this.data = data;
 
         this.init = function () {
-            this.render();
             const parents = this.el.querySelectorAll('[data-parent]')
 
             parents.forEach(parent => {
@@ -107,39 +27,33 @@ function init() {
         }
 
         this.render = function () {
-            this.el.innerHTML = this.renderParent(this.data)
+            if (data != null)
+                this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
         }
 
         this.renderParent = function (data) {
-            let result = "";
             if (data.hasChildren) {
-                result += `
-                    <div class="list-item " data-parent>
+                return `<div class="list-item list-item_open" data-parent>
                         <div class="list-item__inner">
                             <img class="list-item__arrow" src="img/chevron-down.png" alt="chevron-down" data-open>
                             <img class="list-item__folder" src="img/folder.png" alt="folder">
                             <span>${data.name}</span>
                         </div>
-                    <div class="list-item__items">
-                `;
-                for(let item of data.items)
-                    result += this.renderParent(item);
-                result += `</div></div>`;
-            }   
-            else result += this.renderChildren(data);
-            return result;
+                        <div class="list-item__items">
+                            ${data.items.map(element => this.renderParent(element)).join(" ")}
+                        </div>
+                    </div>`
+            }
+            else return this.renderChildren(data)
         }
 
         this.renderChildren = function (data) {
-            return `
-                <div class="list-item list-item_open">
-                    <div class="list-item__inner">
-                        <img class="list-item__folder" src="img/folder.png" alt="folder">
-                        <span>${data.name}</span>
-                    </div>
-                </div>
-                <div class="list-item__items"></div>
-            `
+            return `<div class="list-item">
+                        <div class="list-item__inner">
+                            <img class="list-item__folder" src="img/folder.png" alt="folder">
+                            <span>${data.name}</span>
+                        </div>
+                    </div>`
         }
 
         this.toggleItems = function (parent) {
